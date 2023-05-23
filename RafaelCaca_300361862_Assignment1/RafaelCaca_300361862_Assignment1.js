effc035884f7fbbff486b777b4ec72185581a78a
rafael0427 <134330906+rafael0427@users.noreply.github.com>
2023-05-22 19:47:18 -0700

    Add files via upload

---
+++ RafaelCaca_300361862_Assignment1/RafaelCaca_300361862_Assignment1.js
@@ -0,0 +1,333 @@
+
+    var avatarArray = [
+        {
+            "name": "iboya vat",
+            "image": "https://randomuser.me/api/portraits/thumb/women/67.jpg",
+            "email": "iboya.vat@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "aapo niskanen",
+            "image": "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+            "email": "aapo.niskanen@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "zilda moreira",
+            "image": "https://randomuser.me/api/portraits/thumb/women/38.jpg",
+            "email": "zilda.moreira@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "lilou le gall",
+            "image": "https://randomuser.me/api/portraits/thumb/women/23.jpg",
+            "email": "lilou.legall@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "lucy hall",
+            "image": "https://randomuser.me/api/portraits/thumb/women/24.jpg",
+            "email": "lucy.hall@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "sara alves",
+            "image": "https://randomuser.me/api/portraits/thumb/women/14.jpg",
+            "email": "sara.alves@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "ramon macrae",
+            "image": "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+            "email": "ramon.macrae@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "connor taylor",
+            "image": "https://randomuser.me/api/portraits/thumb/men/15.jpg",
+            "email": "connor.taylor@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "aymeric morel",
+            "image": "https://randomuser.me/api/portraits/thumb/men/11.jpg",
+            "email": "aymeric.morel@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "lorenz otto",
+            "image": "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+            "email": "lorenz.otto@example.com",
+            "joined": "07/15/15"
+        },
+        {
+            "name": "ceylan atakol",
+            "image": "https://randomuser.me/api/portraits/thumb/women/81.jpg",
+            "email": "ceylan.atakol@example.com",
+            "joined": "04/08/07"
+        },
+        {
+            "name": "renata philippe",
+            "image": "https://randomuser.me/api/portraits/thumb/women/11.jpg",
+            "email": "renata.philippe@example.com",
+            "joined": "11/10/09"
+        },
+        {
+            "name": "jose villareal",
+            "image": "https://randomuser.me/api/portraits/thumb/men/30.jpg",
+            "email": "jose.villareal@example.com",
+            "joined": "05/26/02"
+        },
+        {
+            "name": "Abbey king",
+            "image": "https://randomuser.me/api/portraits/thumb/women/46.jpg",
+            "email": "abbey.king@example.com",
+            "joined": "11/25/03"
+        },
+        {
+            "name": "johan petersen",
+            "image": "https://randomuser.me/api/portraits/thumb/women/46.jpg",
+            "email": "johan.petersen@example.com",
+            "joined": "03/07/07"
+        },
+        {
+            "name": "jimi lehtola",
+            "image": "https://randomuser.me/api/portraits/thumb/men/30.jpg",
+            "email": "jimi.lehtola@example.com",
+            "joined": "08/11/15"
+        },
+        {
+            "name": "ielizaveta zagrodskiy",
+            "image": "https://randomuser.me/api/portraits/thumb/women/53.jpg",
+            "email": "ielizaveta.zagrodskiy@example.com",
+            "joined": "10/30/10"
+        },
+        {
+            "name": "mercedes ibarra",
+            "image": "https://randomuser.me/api/portraits/thumb/women/54.jpg",
+            "email": "mercedes.ibarra@example.com",
+            "joined": "09/10/11"
+        },
+        {
+            "name": "cesaltino santos",
+            "image": "https://randomuser.me/api/portraits/thumb/men/70.jpg",
+            "email": "cesaltino.santos@example.com",
+            "joined": "12/26/20"
+        },
+        {
+            "name": "june george",
+            "image": "https://randomuser.me/api/portraits/thumb/women/19.jpg",
+            "email": "june.george@example.com",
+            "joined": "04/15/13"
+        },
+        {
+            "name": "ridge vankoolwijk",
+            "image": "https://randomuser.me/api/portraits/thumb/men/63.jpg",
+            "email": "ridge.vankoolwijk@example.com",
+            "joined": "05/13/09"
+        },
+        {
+            "name": "elli huotari",
+            "image": "https://randomuser.me/api/portraits/thumb/women/81.jpg",
+            "email": "elli.huotari@example.com",
+            "joined": "04/25/02"
+        },
+        {
+            "name": "martin palmer",
+            "image": "https://randomuser.me/api/portraits/thumb/men/8.jpg",
+            "email": "martin.palmer@example.com",
+            "joined": "03/31/09"
+        },
+        {
+            "name": "miriam morales",
+            "image": "https://randomuser.me/api/portraits/thumb/women/61.jpg",
+            "email": "miriam.morales@example.com",
+            "joined": "06/25/20"
+        },
+        {
+            "name": "elizabeth evans",
+            "image": "https://randomuser.me/api/portraits/thumb/women/63.jpg",
+            "email": "elizabeth.evans@example.com",
+            "joined": "07/29/17"
+        },
+        {
+            "name": "melissa fisher",
+            "image": "https://randomuser.me/api/portraits/thumb/women/8.jpg",
+            "email": "melissa.fisher@example.com",
+            "joined": "10/15/12"
+        },
+        {
+            "name": "lawrence newman",
+            "image": "https://randomuser.me/api/portraits/thumb/men/1.jpg",
+            "email": "lawrence.newman@example.com",
+            "joined": "10/18/08"
+        },
+        {
+            "name": "felix christensen",
+            "image": "https://randomuser.me/api/portraits/thumb/men/83.jpg",
+            "email": "felix.christensen@example.com",
+            "joined": "10/29/05"
+        },
+        {
+            "name": "eva dominguez",
+            "image": "https://randomuser.me/api/portraits/thumb/women/3.jpg",
+            "email": "eva.dominguez@example.com",
+            "joined": "07/19/20"
+        },
+        {
+            "name": "jeanne martin",
+            "image": "https://randomuser.me/api/portraits/thumb/women/30.jpg",
+            "email": "jeanne.martin@example.com",
+            "joined": "01/02/07"
+        },
+        {
+            "name": "rostislava pavlyak",
+            "image": "https://randomuser.me/api/portraits/thumb/women/49.jpg",
+            "email": "rostislava.pavlyak@example.com",
+            "joined": "09/14/10"
+        },
+        {
+            "name": "fernando gonzalez",
+            "image": "https://randomuser.me/api/portraits/thumb/men/61.jpg",
+            "email": "fernando.gonzalez@example.com",
+            "joined": "12/11/18"
+        },
+        {
+            "name": "june george",
+            "image": "https://randomuser.me/api/portraits/thumb/women/19.jpg",
+            "email": "june.george@example.com",
+            "joined": "04/15/13"
+        }
+    ];
+    var avatarContainer = document.getElementById('avatarContainer');
+    var paginationContainer = document.getElementById('pagination');
+    var itemsPerPage = 10;
+    var currentPage = 1;
+
+    function displayAvatars(page) {
+        avatarContainer.innerHTML = '';
+        var startIndex = (page - 1) * itemsPerPage;
+        var endIndex = startIndex + itemsPerPage;
+
+        for (var i = startIndex; i < endIndex && i < avatarArray.length; i++) {
+            var avatar = avatarArray[i];
+            var listItem = document.createElement('li');
+            listItem.className = 'contact-item cf';
+
+            var contactDetails = document.createElement('div');
+            contactDetails.className = 'contact-details';
+
+            var avatarImage = document.createElement('img');
+            avatarImage.className = 'avatar';
+            avatarImage.src = avatar.image;
+            avatarImage.alt = avatar.name;
+
+            var avatarName = document.createElement('h3');
+            avatarName.textContent = avatar.name;
+
+            var avatarEmail = document.createElement('span');
+            avatarEmail.className = 'email';
+            avatarEmail.textContent = avatar.email;
+
+            contactDetails.appendChild(avatarImage);
+            contactDetails.appendChild(avatarName);
+            contactDetails.appendChild(avatarEmail);
+
+            var joinedDetails = document.createElement('div');
+            joinedDetails.className = 'joined-details';
+
+            var avatarDate = document.createElement('span');
+            avatarDate.className =  'joined';
+            avatarDate.textContent ='Joined: ' + avatar.joined;
+
+            joinedDetails.appendChild(avatarDate);
+
+            listItem.appendChild(contactDetails);
+            listItem.appendChild(joinedDetails);
+
+            avatarContainer.appendChild(listItem);
+        }
+    }
+
+    function displayPagination() {
+        paginationContainer.innerHTML = '';
+        var totalPages = Math.ceil(avatarArray.length / itemsPerPage);
+
+        for (var i = 1; i <= totalPages; i++) {
+            var pageButton = document.createElement('button');
+            pageButton.textContent = i;
+            pageButton.addEventListener('click', function () {
+                currentPage = parseInt(this.textContent);
+                displayAvatars(currentPage);
+            });
+
+            paginationContainer.appendChild(pageButton);
+        }
+    }
+
+    displayAvatars(currentPage);
+    displayPagination();
+
+    
+    function main(pageNum) {
+        var nextPage = display * pageNum;
+        var prevPage = display * (pageNum - 1);
+        for (let i = 0; i < li.length; i++) {
+            li[i].style.display = "none";
+            if (i < nextPage && i >= prevPage) {
+                li[i].style.display = "block";
+            }
+        }
+    }
+
+    function displayPagination() {
+        paginationContainer.innerHTML = '';
+        var totalPages = Math.ceil(avatarArray.length / itemsPerPage);
+
+        var prevButton = document.createElement('button');
+        prevButton.textContent = 'Prev';
+        prevButton.addEventListener('click', function () {
+            if (currentPage > 1) {
+                currentPage--;
+                displayAvatars(currentPage);
+                highlightActiveButton(this);
+            }
+        });
+
+        paginationContainer.appendChild(prevButton);
+
+        for (var i = 1; i <= totalPages; i++) {
+            var pageButton = document.createElement('button');
+            pageButton.textContent = i;
+            pageButton.addEventListener('click', function () {
+                currentPage = parseInt(this.textContent);
+                displayAvatars(currentPage);
+                highlightActiveButton(this);
+            });
+
+            paginationContainer.appendChild(pageButton);
+        }
+
+        var nextButton = document.createElement('button');
+        nextButton.textContent = 'Next';
+        nextButton.addEventListener('click', function () {
+            if (currentPage < totalPages) {
+                currentPage++;
+                displayAvatars(currentPage);
+                highlightActiveButton(this);
+            }
+        });
+
+        paginationContainer.appendChild(nextButton);
+
+        highlightActiveButton(null);
+    }
+
+    /* REFERENCES
+    W3Schools
+	• https://www.w3schools.com/js/js_dom_examples.asp
+
+    TylerPotts - Pagination in JavaScript
+	• https://www.youtube.com/tylerpotts
+		○ Pagination in JavaScript
+        https://youtu.be/IqYiVHrO2U8 */
